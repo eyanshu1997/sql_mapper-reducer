9fdcdf138a696c96f1eff222be87f17595f48ee2
aurav <eyanshu1997@gmail.com>
2020-09-26 05:14:21 +0530

    reviews table don

--- mapper.py
+++ mapper.py
@@ -5,7 +5,7 @@ import re
 #  SELECT group, COUNT(group) FROM products GROUP BY group HAVING COUNT(group)>3
 #f=open("resultjson.txt")
 import json
-i=0;
+
 f2=open("/home/aurav/code/python/hadoop/pro/mapperip.txt")
 l=f2.readline()
 config=json.loads(l)
@@ -23,6 +23,7 @@ hav=config['agg']
 #print(agg_col)
 wlhs=config['wlhs']
 wo=config['wo']
+table=config["tables"]
 wrhs=config['wrhs']
 intval=["PID","VOTES","HELPFUL","RATING","SALESRANK"]
 
@@ -33,34 +34,62 @@ for x in sel:
 	if hav not in x:
 		if x!= agg_col:
 			se.append(x)
-			
+#i=0;	
 for li in sys.stdin:
-	if(i==200):
-		exit()
+#	if(i==200):
+#		exit()
+#	i=i+1
 	line=json.loads(li)
-	i=i+1
+
 	#print(line)
 	if line['TITLE']=="discontinued product":
 		continue
-	x=None
-	y=None
-	if(wlhs in intval):
-		x=int(line[wlhs])
-		y=int(wrhs)
+	if(table=="REVIEWS"):
+		li=line['REVIEWS']
+		for line in li:
+			x=None
+			y=None
+			#print(line)
+			if(wlhs in intval):
+				x=int(line[wlhs])
+				y=int(wrhs)
+			else:
+				x=str(line(wlhs))
+				y=str(wrhs)
+			if(not oper(x,y,wo)):
+				continue	
+			sel_cols = [line[x] for x in se]
+			agg_cols = line[agg_col]
+			if(hav=="*"):
+				ha="*"
+			else:
+				ha=line[hav]
+			if(len(se)==0):
+				print("{}###{}\t{}".format("*",ha, agg_cols))
+			else:
+				print("{}###{}\t{}".format(",".join(sel_cols),ha, agg_cols))
 	else:
-		x=str(line(wlhs))
-		y=str(wrhs)
-	if(not oper(x,y,wo)):
-		continue	
-	sel_cols = [line[x] for x in se]
-	agg_cols = line[agg_col]
-	if(hav=="*"):
-		ha="*"
-	else:
-		ha=line[hav]
-	if(len(se)==0):
-		print("{}###{}\t{}".format("*",ha, agg_cols))
-	else:
-		print("{}###{}\t{}".format(",".join(sel_cols),ha, agg_cols))
+		print(line)
+		x=None
+		y=None
+		print(wlhs)
+		if(wlhs in intval):
+			x=int(line[wlhs])
+			y=int(wrhs)
+		else:
+			x=str(line(wlhs))
+			y=str(wrhs)
+		if(not oper(x,y,wo)):
+			continue	
+		sel_cols = [line[x] for x in se]
+		agg_cols = line[agg_col]
+		if(hav=="*"):
+			ha="*"
+		else:
+			ha=line[hav]
+		if(len(se)==0):
+			print("{}###{}\t{}".format("*",ha, agg_cols))
+		else:
+			print("{}###{}\t{}".format(",".join(sel_cols),ha, agg_cols))
 
 # query = 'SELECT MOVIEID,SUM(RATING) FROM RATING GROUPBY MOVIEID HAVING SUM(RATING) > 1000'
