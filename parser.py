f7e17628298d23123d8017a0596a23bbd04cd878
aurav <eyanshu1997@gmail.com>
2020-09-25 15:06:44 +0530

    initial commit

---
+++ parser.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+import sqlparse
+import re
+import json
+
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SECRET_KEY'] = 'hello'
+
+query = ""
+ps = reqparse.RequestParser()
+ps.add_argument('query')
+def write_to_file(data):
+	fil=open("mapperip.txt","w")
+	json.dump(data,fil)
+	fil.close()
+	return data
+def extractlhs_and_rhs(exp):
+	al_op = ['<=', '<', '=', '>', '>=']
+	for o in al_op:
+		if exp.find(o)!=-1:
+			rhs,lhs=exp.split(o)
+			return {"lhs":lhs.rstrip(),"op":o,"rhs":rhs.rstrip()}
+		
+def parse_query(query):
+	parsed=sqlparse.parse(query.upper())
+	stmt=parsed[0]
+	columns=stmt.tokens[2]
+	tables=stmt.tokens[6]
+	where=stmt.tokens[8]
+	compar=stmt.tokens[-1]
+	where=str(where)
+	whe,group=re.split("GROUPBY ",where)
+	wh,where=whe.split()
+	columns=str(columns)
+	columns=columns.split(",")
+	column=[]
+	compar=extractlhs_and_rhs(str(compar))
+	where=extractlhs_and_rhs(str(where))
+	for co in columns:
+		column.append(co)
+	#print(column)
+	#print(tables)
+	#print(where)
+	#print(group)
+	#print(compar)
+	return write_to_file( {"columns":str(column),"tables":str(tables),"where":str(where),"group":str(group),"compar":str(compar)})
+	
+
+class RunQuery(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        args = ps.parse_args()
+        query = args['query']
+        return parse_query(query)
+api.add_resource(RunQuery, '/query')
+app.run(debug=True)
